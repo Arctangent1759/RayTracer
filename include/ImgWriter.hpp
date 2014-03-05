92381fea13996c5ba315a9ebeff36e4dd0661bcf
Leo Wu <leowu@berkeley.edu>
2014-03-05 13:36:26 -0800

    png writer

--- include/ImgWriter.hpp
+++ include/ImgWriter.hpp
@@ -3,38 +3,41 @@
 
 #include "Color.hpp"
 
+#include "png++/png.hpp"
+
 class ImgWriter{
-    public:
+  public:
 
-        /**
-         *
-         * Creates an image writer for an width by height image
-         * @parm width the width of the image to be written
-         * @parm height the height of the image to be written
-         * 
-         **/
-        ImgWriter(int width, int height);
+    /**
+     *
+     * Creates an image writer for an width by height image
+     * @parm width the width of the image to be written
+     * @parm height the height of the image to be written
+     * 
+     **/
+    ImgWriter(int width, int height);
 
-        /**
-         *
-         * Sets the pixel located at (x,y) to color c.
-         *
-         * @param x the x coordinate of the pixel, on range [0,width)
-         * @param y the y coordinate of the pixel, on range [0,height)
-         * @param c the c color pixel. Contains floats x,y,z.
-         *
-         **/
-        void setPixel(int x,int y, const Color c);
+    /**
+     *
+     * Sets the pixel located at (x,y) to color c.
+     *
+     * @param x the x coordinate of the pixel, on range [0,width)
+     * @param y the y coordinate of the pixel, on range [0,height)
+     * @param c the c color pixel. Contains floats x,y,z.
+     *
+     **/
+    void setPixel(int x,int y, const Color c);
 
-        /**
-         *
-         * Writes the image to the file located at filepath.
-         * @param filepath the file to write to
-         *
-         **/
-        void write(string fileName);
-    protected:
-        //Use this space as needed. Just implement the interface.
+    /**
+     *
+     * Writes the image to the file located at filepath.
+     * @param filepath the file to write to
+     *
+     **/
+    void write(string fileName);
+  protected:
+    //Use this space as needed. Just implement the interface.
+    png::image<png::rgb_pixel> picture;
 };
 
 #endif
--- src/ImgWriter.cpp
+++ src/ImgWriter.cpp
@@ -1,22 +1,23 @@
 #include "ImgWriter.hpp"
 
 //Include your libraries here
+using namespace png;
 
 //TODO: Your helper function preambles here
 
 ImgWriter::ImgWriter(int width, int height){
-    //TODO: Implement ImgWriter here
-
+  //TODO: Implement ImgWriter here
+  picture = image<rgb_pixel>(width, height);
 }
 
 void ImgWriter::setPixel(int x, int y, const Color c){
-    //TODO: Implement setPixel here
-
+  //TODO: Implement setPixel here
+  picture[picture.get_height()-1-y][x] = rgb_pixel(c.getR(), c.getG(), c.getB());
 }
 
 void ImgWriter::write(string filepath){
-    //TODO: Implement write here
-
+  //TODO: Implement write here
+  picture.write(filepath);
 }
 
 //TODO: Your helper functions here
